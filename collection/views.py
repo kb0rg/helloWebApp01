83f0b330a86f6fd27fa8366c4810412958789b77
kb0rg <kristenmborges@gmail.com>
2016-03-26 14:02:51 -0700

    more fun with humanize

--- collection/views.py
+++ collection/views.py
@@ -5,9 +5,13 @@ def index(request):
     numA = 5
     numB = 1500
     numC = 100000000
+    thing = "Thing name"
+    word = "sphygmomanometer"
     return render(request, 
                     'index.html', 
                     {'numA': numA,
                     'numB': numB,
                     'numC': numC,
+                    'thing': thing,
+                    'word': word,
                     })
