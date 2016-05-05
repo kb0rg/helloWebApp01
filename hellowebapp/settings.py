a54334161f87fc47143e7b26654ee485e8a49fd0
kb0rg <kristenmborges@gmail.com>
2016-05-04 22:51:24 -0700

    Remove and make new secret as env var.

--- hellowebapp/settings.py
+++ hellowebapp/settings.py
@@ -19,12 +19,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#eih_%3^36116!hja^)+s7sc_0^v6)$zc(eo)*rpqqnpfs6wit'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 # TODO: Update allowed hosts before deploy!!!
+# Or not? This looks ok according to code on:
+# https://devcenter.heroku.com/articles/getting-started-with-python#prepare-the-app
 ALLOWED_HOSTS = ['*']
 
 
