c3beb33ceabd62b4c018c856a1e0a0c020c29657
kb0rg <kristenmborges@gmail.com>
2016-05-04 21:38:52 -0700

    Update for WhiteNoise.

--- hellowebapp/wsgi.py
+++ hellowebapp/wsgi.py
@@ -1,3 +1,13 @@
+import os
+
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hellowebapp.settings")
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
+
 """
 WSGI config for hellowebapp project.
 
@@ -6,11 +16,3 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
-
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hellowebapp.settings")
-
-application = get_wsgi_application()
